added in log scaling, more commenting, fixed the weights problem that carter caught

q8.py
@@ -17,7 +17,7 @@ import PlotUtilities as pPlotUtil
 import CheckpointUtilities as pCheckUtil
 
 def getRandomDNA(chars,dnaLen,numToChoose,weights):
-    rawArr = np.random.choice(chars,size=(numToChoose,dnaLen))
+    rawArr = np.random.choice(chars,size=(numToChoose,dnaLen),p=weights)
     # POST: rawArr[i][j] has character [j] of dna string [i] 
     # (ie: each row is a string)
     # combine all the columns in a given row for the actual strings
@@ -26,7 +26,10 @@ def getKmers(string,k):
     return [ string[i:i+k] for i in range(0,len(string)-k+1)]
     
 def getMinK(seqs):
+    # get the minimum integer k such that each -mer occurs once in each of seqs
+    # note: we will use '-1' to note 'we haven't found the minimum k yet'
     arrK =np.ones(numOligos,dtype=np.uint32) * -1
+    # use the following two bookkeeping variables to help with looping
     kNotFoundNum = arrK.size
     goodIdx =np.where(arrK < 0)[0]
     kmer = 1
@@ -34,27 +37,30 @@ def getMinK(seqs):
         working = seqs[goodIdx]
         # transform the dna into their kmers
         kmers = map(lambda x: getKmers(x,kmer),working)
-        # get the count of each kmers occurance
+        # get the set of each of the kmers
         kmerSet = map(set,kmers)
-        # For each set of kmers kmerSet[i], count occurences in kmers[i]
+        # For each set of kmers (kmerSet[i]), count occurences in kmers[i]
         kmerCounts = [ [ kmers[i].count(c) for c in tmpSet ] \
                        for i,tmpSet in enumerate(kmerSet) ]
-        # get the maximum count 
+        # maximum count (ie: the maximum number of times *any* kmer happens
         maxCount = map(max,kmerCounts)
         # get the index where the max was one (ie: at most 1 of the kmers)
         bestIdx = [ goodIdx[i] for i,c in enumerate(maxCount)  if c==1]
+        # update the bookkeeping stuff
         arrK[bestIdx] = kmer
         goodIdx =np.where(arrK < 0)[0]
         kNotFoundNum = goodIdx.size
-        print(kNotFoundNum)
         kmer += 1
     return arrK
 
 def getKSequence(lenArr,numOligos,weights,chars):
     toRet = []
     for idx,lenV in enumerate(lenArr):
+        # get the random DNA according to the lengths we have, the chars/weights
         dna = getRandomDNA(chars,lenV,numOligos,weights)
+        # append the minimum k such that each kmer appears at most once
         toRet.append(getMinK(dna))
+        # progress bar! 
         print("{:d}/{:d}".format(idx+1,len(lenArr)))
     return toRet
 
@@ -86,24 +92,25 @@ chars = table[0]
 weights = table[1]
 q = max(weights)
  # use for easy python string generation
-# POST: bag has the characters in their appropriate proportion
 numOligos = 10e3
-lengths = np.array([32,64,128,256,512])
+lengths = np.array([4,8,16,32,64,128,256,512])
 # save the K array: minimum k to have at most one k-mer
 # initialize to -1, so that we know when we have the minimum
 outDir = "./out/"
 pGenUtil.ensureDirExists(outDir)
-
+# use checkpointing to save data, since it takes forever
 kArr = pCheckUtil.getCheckpoint('./tmp/check.pkl',getKSequence,False,
                                 lengths,numOligos,weights,chars)
 meanVals,std = pCheckUtil.getCheckpoint('./tmp/meanStd.pkl',plotAll,False,kArr,
                                     outDir)
-
+# plot the mean k vs dna length, l (in theory, k is approx log_1/q(l+1))
 fig = pPlotUtil.figure()
+ax = plt.subplot(1,1,1)
 plt.errorbar(x=lengths,y=meanVals,yerr=std,fmt='ro-',label='Mean K')
-plt.plot(lengths,np.log(lengths+1)/np.log(1/q),'b--',label='Log_[1/q](l+1)')
+plt.plot(lengths,np.log(lengths+1)/np.log(1./q),'b--',label='Log_[1/q](l+1)')
 plt.xlabel('DNA Length (l)')
 plt.ylabel('Mean K value')
 plt.title('Mean K vs length')
+ax.set_xscale('log')
 plt.legend(loc='best')
 pPlotUtil.savefig(fig,outDir + 'k_v_len')